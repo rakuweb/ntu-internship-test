29be3b6b1dcd2be7d5f6a868e767286fe359578d
snow-faluna <faluna@snowhite.co.jp>
2022-02-20 12:44:12 +0900

    feat:tada: Make .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    commonjs: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  plugins: ["react-hooks", "react", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "next",
+    "next/core-web-vitals",
+    "prettier",
+  ],
+  rules: {
+    "react/prop-types": "off",
+  },
+};
