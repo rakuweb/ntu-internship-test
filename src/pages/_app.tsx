a5c8ff85722a36c3046f7555b1fab80f019dbe7f
snow-faluna <faluna@snowhite.co.jp>
2022-02-20 15:16:54 +0900

    feat:sparkles: Move styles into src

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { NextSeo, DefaultSeo } from 'next-seo';
+import { DefaultSeo } from 'next-seo';
 
 import { SEO } from '~/constants/seo';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
