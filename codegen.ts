6db425e8b8a86a8b88b1f95821f3d49a64f03f57
shoma <takano@rakuweb.jp>
2023-09-14 15:00:59 +0900

    codegen部分変更

--- codegen.ts
+++ codegen.ts
@@ -1,9 +1,9 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  overwrite: true,
-  schema: 'http://118.27.25.166:1337/graphql',
-  documents: 'src/**/*.graphql',
+  overwrite: false,
+  schema: 'https://management.nottheuniversity.com/gql',
+  documents: 'src/lib/graphql/**',
   generates: {
     'src/types/gql/': {
       preset: 'client',
--- offercodegen.ts
+++ offercodegen.ts
@@ -1,11 +1,13 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  overwrite: true,
-  schema: 'http://localhost:1337/graphql',
+  overwrite: false,
+  schema: 'http://118.27.25.166:1337/graphql',
   documents: [
-    'src/lib/graphql/offer/index.graphql',
-    'src/lib/graphql/offers/index.graphql',
+    'src/lib/offers-graphql/offer/index.graphql',
+    'src/lib/offers-graphql/offers/index.graphql',
+    'src/lib/offers-graphql/point/index.graphql',
+    'src/lib/offers-graphql/points/index.graphql',
   ],
   generates: {
     'src/types/gql/': {
